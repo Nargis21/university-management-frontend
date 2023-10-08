abe5ea21672ea31ac032bbe3cfa125f9495d3d6e
nargis21 <nargisakther1933@gmail.com>
2023-10-08 19:20:50 +0600

    dynamuc role based sidebar added

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -9,6 +9,8 @@ import {
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Layout, Menu, } from 'antd';
+import { USER_ROLE } from '@/constants/role';
+import { sidebarItems } from '@/constants/sidebarItems';
 const { Sider } = Layout;
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -40,12 +42,13 @@ const items: MenuItem[] = [
 
 const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const role = USER_ROLE.ADMIN
     return (
         <Sider width={280} style={{ overflow: 'auto', height: '100vh', position: 'sticky', left: '0', top: '0', bottom: '0' }} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
             <div style={{ color: 'white', fontSize: '2rem', textAlign: 'center', fontWeight: 'bold', marginBottom: '1rem' }}>
                 PH University
             </div>
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={sidebarItems(role)} />
         </Sider>
     );
 };
