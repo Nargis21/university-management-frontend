4a77472535bca6a22f60c7b6a628a31423af2f6d
nargis21 <nargisakther1933@gmail.com>
2023-10-10 12:42:36 +0600

    breadCrumb added

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,10 +1,22 @@
 'use client'
 import { Layout } from 'antd';
+import UMBreadCrumb from './UMBreadCrumb';
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
+    const base = 'admin'
     return (
         <Content style={{ minHeight: '100vh' }}>
+            <UMBreadCrumb items={[
+                {
+                    label: `${base}`,
+                    link: `/${base}`
+                },
+                {
+                    label: 'student',
+                    link: `/${base}/student`
+                },
+            ]}></UMBreadCrumb>
             {children}
         </Content>
     );
